0ea763c0fec1b64258d92ed1782335c3013cdb61
kusfreshforces <khussalz@fresh-forces.com>
2019-11-26 11:31:36 +0700

    add insertDocuments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+const insertDocuments = function (db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    { a: 1 }, { a: 2 }, { a: 3 }
+  ], function (err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+const findDocuments = function (db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function (err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* POST home page. */
+router.post('/', function (req, res) {
+  MongoClient.connect(url, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    insertDocuments(db, function () {
+      client.close();
+    });
+  });
+  res.render('index', { title: 'Express' });
+})
 
 module.exports = router;
